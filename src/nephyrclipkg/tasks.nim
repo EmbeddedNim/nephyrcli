7fe0637e502dc073aa2de1298b4b4f557168dbd8
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-05-03 22:29:24 -0700

    updates

--- src/nephyrclipkg/tasks.nim
+++ src/nephyrclipkg/tasks.nim
@@ -317,7 +317,7 @@ task zDepsClone, "clone Nephyr deps":
   echo fmt"work: {projectDir()=}"
   echo fmt"work: {getCurrentDir()=}"
   echo fmt"work: {srcDir=}"
-  let pkgDir = nopts.projDir / ".. " / ".." / "packages/"
+  let pkgDir = nopts.projDir & "../../packages/"
   echo fmt"work: {pkgDir=}"
   withDir(pkgDir):
     for dep in ["mcu_utils", "fastrpc", "nephyr"]:
